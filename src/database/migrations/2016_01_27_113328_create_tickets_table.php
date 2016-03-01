32898eec0ae6d9da83af9cac9bb198bf1e206206
amin <aminmazrouei@gmail.com>
2016-03-01 20:38:42 +0330

    fixed Ticket class name

--- src/database/migrations/2016_01_27_113328_create_tickets_table.php
+++ src/database/migrations/2016_01_27_113328_create_tickets_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTicketQuestionsTable extends Migration
+class CreateTicketTable extends Migration
 {
     /**
      * Run the migrations.
