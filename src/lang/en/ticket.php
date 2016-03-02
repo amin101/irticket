89720d1fdc2525a86c5fe23449fec2d55cd9521e
Amin Mazrouei <aminmazrouei@gmail.com>
2016-03-02 17:57:40 +0330

    added en translation

---
+++ src/lang/en/ticket.php
@@ -0,0 +1,23 @@
+<?php
+
+
+return [
+
+	'open' => 'open',
+	'resolved' => 'resolved',
+	'status' => 'status',
+	'index-title' => 'title',
+	'submit' => 'submit',
+	'close_issue' => 'close issue',
+	'created_by' => 'created by',
+	'on' => 'on',
+    'updated-at' => 'updated at',
+    'delete' => 'delete',
+    'user' => 'user',
+    'title' => 'title',
+	'no-ticket-msg' => 'You have not created any ticket',
+	'create-new-ticket-msg' => 'create a new one',
+	'new-ticket' => 'new ticket',
+	
+
+];
